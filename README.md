e9954e8f49b2eef73eb21f304440efbe9706e819
Kyle <kwaldner3@gatech.edu>
2020-02-28 20:45:05 -0500

    use ag for search C-c i

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 [![Build Status](https://travis-ci.org/purcell/emacs.d.svg?branch=master)](https://travis-ci.org/purcell/emacs.d)
-<a href="https://www.patreon.com/sanityinc"><img alt="Support me" src="https://img.shields.io/badge/Support%20Me-%F0%9F%92%97-ff69b4.svg"></a>
+
+# important notes
+install ag (silversurfer) on system to use ag for search
+
+
+
+
 
 # A reasonable Emacs config
 
--- lisp/init-grep.el
+++ lisp/init-grep.el
@@ -8,24 +8,32 @@
 (when *is-a-mac*
   (setq-default locate-command "mdfind"))
 
+(defun search-refresh-key (this-mode-map)
+  "Set refresh key for this type of search mode.  THIS-MODE-MAP is the mode map to set."
+  (dolist (key (list (kbd "C-c C-r") (kbd "r")))
+    (define-key this-mode-map key 'revert-buffer)))
+
 (require-package 'wgrep)
 (after-load 'grep
   (dolist (key (list (kbd "C-c C-q") (kbd "w")))
     (define-key grep-mode-map key 'wgrep-change-to-wgrep-mode))
-  (dolist (key (list (kbd "C-c C-r") (kbd "r")))
-    (define-key grep-mode-map key 'revert-buffer)))
+  (search-refresh-key grep-mode-map))
 
 (when (and (executable-find "ag")
            (maybe-require-package 'ag))
-  (require-package 'wgrep-ag)
+  (maybe-require-package 'wgrep-ag)
+  ;; C-C C-p to change to wgrep mode
   (setq-default ag-highlight-search t)
-  (global-set-key (kbd "M-?") 'ag-project))
+  ;; (search-refresh-key ag-mode-map) ;; ag-mode-map does not exist
+  ;; use g to refresh buffer
+  (global-set-key (kbd "C-c i") 'ag-project)) ;; global search in project
 
 (when (and (executable-find "rg")
            (maybe-require-package 'rg))
   (maybe-require-package 'deadgrep)
   (global-set-key (kbd "M-?") 'rg-project))
 
+;; grep-find-ignored-directories needs to be modified for rgrep to e useful
 
 (provide 'init-grep)
 ;;; init-grep.el ends here
