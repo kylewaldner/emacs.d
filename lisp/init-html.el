d4e09f6a9ccef3bbfdef90dc85c655c550f0e99f
kyle <kyle@kyle-debian.gatech.edu>
2020-03-22 23:11:03 -0400

    web mode

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -5,14 +5,14 @@
 
 ;;; Code:
 
-(require-package 'tagedit)
-(after-load 'sgml-mode
-  (tagedit-add-paredit-like-keybindings)
-  (define-key tagedit-mode-map (kbd "M-?") nil)
-  (define-key tagedit-mode-map (kbd "M-s") nil)
-  (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1))))
+;; (require-package 'tagedit)
+;; (after-load 'sgml-mode
+;;   (tagedit-add-paredit-like-keybindings)
+;;   (define-key tagedit-mode-map (kbd "M-?") nil)
+;;   (define-key tagedit-mode-map (kbd "M-s") nil)
+;;   (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1))))
 
-(add-auto-mode 'html-mode "\\.\\(jsp\\|tmpl\\)\\'")
+;; (add-auto-mode 'html-mode "\\.\\(jsp\\|tmpl\\)\\'")
 
 
 (provide 'init-html)
--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -7,12 +7,14 @@
 (maybe-require-package 'coffee-mode)
 (maybe-require-package 'typescript-mode)
 (maybe-require-package 'prettier-js)
+(maybe-require-package 'web-mode)
 (maybe-require-package 'npm-mode)
 
 ;; Need to first remove from list if present, since elpa adds entries too, which
 ;; may be in an arbitrary order
 
 (add-to-list 'auto-mode-alist '("\\.\\(js\\|es6\\)\\(\\.erb\\)?\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
 ;; js2-mode
 
