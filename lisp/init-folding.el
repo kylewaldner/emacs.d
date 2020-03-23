1b2193cd67c5e5fe6d2423990d56d9167905885c
kyle <kyle@kyle-debian.gatech.edu>
2020-03-23 01:31:30 -0400

    use hs-minor mode for folding

--- lisp/init-folding.el
+++ lisp/init-folding.el
@@ -2,15 +2,23 @@
 ;;; Commentary:
 ;;; Code:
 
-(when (maybe-require-package 'origami)
-  (after-load 'origami
-    ;; (define-key origami-mode-map (kbd "C-c C-a") 'origami-recursively-toggle-node)
-    ;; (define-key origami-mode-map (kbd "C-c C-A") 'origami-toggle-all-nodes)
-    ;; find better folding library in future
-    (define-key origami-mode-map (kbd "C-c [") 'origami-open-node)
-    (define-key origami-mode-map (kbd "C-c ]") 'origami-close-node))
-  (add-hook 'prog-mode-hook 'origami-mode))
+;; (when (maybe-require-package 'origami)
+;;   (after-load 'origami
+;;     ;; (define-key origami-mode-map (kbd "C-c C-a") 'origami-recursively-toggle-node)
+;;     ;; (define-key origami-mode-map (kbd "C-c C-A") 'origami-toggle-all-nodes)
+;;     ;; find better folding library in future
+;;     (define-key origami-mode-map (kbd "C-c [") 'origami-open-node)
+;;     (define-key origami-mode-map (kbd "C-c ]") 'origami-close-node))
+;;   (add-hook 'prog-mode-hook 'origami-mode))
 
+;; (when (maybe-require-package 'fold-dwim)
+;;   (global-set-key (kbd "C-x a a") 'fold-dwim-toggle)
+;;   (global-set-key (kbd "C-x a s") 'fold-dwim-hide-all)
+;;   (global-set-key (kbd "C-x a d") 'fold-dwim-show-all))
+
+;; C-x a (a s d f)
+
+(add-hook 'prog-mode-hook 'hs-minor-mode)
 
 (provide 'init-folding)
 ;;; init-folding.el ends here
