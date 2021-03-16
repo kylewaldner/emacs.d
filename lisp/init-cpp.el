08e5b4ac6a228ed1e266eca933e2c4ce5de1a21e
Kyle Waldner <dev@kylewaldner.com>
2021-03-16 00:12:50 -0400

    fixed cpp config

--- lisp/init-cpp.el
+++ lisp/init-cpp.el
@@ -7,8 +7,10 @@
 
 ;;  If you are using Cmake to build your project, it is really easy to generate compilation database, since CMake has support for it. You just provide CMake with correct flag (cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ...) and that is it! Later you can just call cmake . and it will update compilation database if needed. This is what I used for my project, since it uses CMake.
 
-(when (require 'irony)
+
+(when (maybe-require-package 'irony)
   ;; If irony server was never installed, install it.
+  (require 'irony)
   (unless (irony--find-server-executable) (call-interactively #'irony-install-server))
   (add-hook 'c++-mode-hook 'irony-mode)
   (add-hook 'c-mode-hook 'irony-mode)
@@ -17,59 +19,64 @@
   (setq-default irony-cdb-compilation-databases '(irony-cdb-libclang
                                                   irony-cdb-clang-complete))
 
-  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+  (when (and (maybe-require-package 'company-irony) (maybe-require-package 'company-irony-c-headers))
+    (eval-after-load 'company '(add-to-list 'company-backends '(company-irony-c-headers company-irony))))
 
-(when (and (maybe-require-package 'company-irony) (maybe-require-package 'company-irony-c-headers))
-  (eval-after-load 'company '(add-to-list 'company-backends '(company-irony-c-headers company-irony))))
 
+  (when (maybe-require-package 'flycheck-irony)
+    (eval-after-load 'flycheck '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup)))
 
-(when (maybe-require-package 'flycheck-irony)
-  (eval-after-load 'flycheck '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup)))
+  (when (maybe-require-package 'irony-eldoc)
+    (add-hook 'irony-mode-hook #'irony-eldoc))
+  )
 
-(when (maybe-require-package 'irony-eldoc)
-  (add-hook 'irony-mode-hook #'irony-eldoc))
-
+;; (when (maybe-require-package 'cmake-ide)
+;;   (cmake-ide-setup))
 
 (setq kyle/use-rtags nil)
 
-(require 'cc-mode)
+;; (require 'cc-mode)
 
-(when (maybe-require-package 'rtags)
-  ;; (unless (rtags-executable-find "rc") (error "Binary rc is not installed"))
-  ;; (unless (rtags-executable-find "rc") (error "Binary rc is not installed"))
-  (define-key c-mode-base-map (kbd "M-.") 'rtags-find-symbol-at-point)
-  (define-key c-mode-base-map (kbd "M-,") 'rtags-find-references-at-point)
-  (define-key c-mode-base-map (kbd "M-?") 'rtags-display-summary)
-  (rtags-enable-standard-keybindings)
-  (when (maybe-require-package 'company-rtags)
-    (setq rtags-autostart-diagnostics t)
-    (rtags-diagnostics)
-    (setq rtags-completions-enabled t)
-    (eval-after-load 'company '(add-to-list 'company-backends 'company-rtags))
-    )
-  )
+;; (require-package 'rtags)
 
-
-(when (and (maybe-require-package 'flycheck-rtags) kyle/use-rtags)
-  ;; ensure that we use only rtags checking
-  ;; https://github.com/Andersbakken/rtags#optional-1
-  (defun setup-flycheck-rtags ()
-    (flycheck-select-checker 'rtags)
-    (setq-local flycheck-highlighting-mode nil) ;; RTags creates more accurate overlays.
-    (setq-local flycheck-check-syntax-automatically nil)
-    (rtags-set-periodic-reparse-timeout 2.0) ;; Run flycheck 2 seconds after being idle.
-    )
-  (add-hook 'c-mode-hook #'setup-flycheck-rtags)
-  (add-hook 'c++-mode-hook #'setup-flycheck-rtags))
+;; (when (require 'rtags)
+;;   ;; (unless (rtags-executable-find "rc") (error "Binary rc is not installed"))
+;;   ;; (unless (rtags-executable-find "rc") (error "Binary rc is not installed"))
+;;   (define-key c-mode-base-map (kbd "M-.") 'rtags-find-symbol-at-point)
+;;   (define-key c-mode-base-map (kbd "M-,") 'rtags-find-references-at-point)
+;;   (define-key c-mode-base-map (kbd "M-?") 'rtags-display-summary)
+;;   (rtags-enable-standard-keybindings)
+;;   (when (maybe-require-package 'company-rtags)
+;;     (setq rtags-autostart-diagnostics t)
+;;     (rtags-diagnostics)
+;;     (setq rtags-completions-enabled t)
+;;     (eval-after-load 'company '(add-to-list 'company-backends 'company-rtags))
+;;     )
+;;   )
 
 
-(defun kyle/c-project-setup ()
-  "Setup a the c project by generating compile commands with bear."
-  (interactive)
-  (projectile-with-default-dir (projectile-ensure-project (projectile-project-root))
-    (shell-command "bear make"))
-  (call-interactively 'irony-cdb-json-add-compile-commands-path)
-  )
+;; (when (and (maybe-require-package 'flycheck-rtags) kyle/use-rtags)
+;;   ;; ensure that we use only rtags checking
+;;   ;; https://github.com/Andersbakken/rtags#optional-1
+;;   (defun setup-flycheck-rtags ()
+;;     (flycheck-select-checker 'rtags)
+;;     (setq-local flycheck-highlighting-mode nil) ;; RTags creates more accurate overlays.
+;;     (setq-local flycheck-check-syntax-automatically nil)
+;;     (rtags-set-periodic-reparse-timeout 2.0) ;; Run flycheck 2 seconds after being idle.
+;;     )
+;;   (add-hook 'c-mode-hook #'setup-flycheck-rtags)
+;;   (add-hook 'c++-mode-hook #'setup-flycheck-rtags))
+
+
+;; (defun kyle/c-project-setup ()
+;;   "Setup a the c project by generating compile commands with bear."
+;;   (interactive)
+;;   (projectile-with-default-dir (projectile-ensure-project (projectile-project-root))
+;;     (shell-command "bear make"))
+;;   (call-interactively 'irony-cdb-json-add-compile-commands-path)
+;;   ;; (call-interactively 'irony-cdb-json-select)
+;;   )
 
 
 (provide 'init-cpp)
