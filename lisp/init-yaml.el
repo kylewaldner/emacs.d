9e881fe4acced84f44ad673f6656ae94419c4c92
Kyle Waldner <kyle.waldner@includedhealth.com>
2025-08-07 14:05:32 -0400

    add indentation highlighting for yaml mode

--- lisp/init-yaml.el
+++ lisp/init-yaml.el
@@ -6,5 +6,9 @@
 (add-auto-mode 'yaml-mode "\\.yml\\.erb\\'")
 (add-hook 'yaml-mode-hook 'goto-address-prog-mode)
 
+;; Indentation highlighting
+(require 'highlight-indentation)
+(add-hook 'yaml-mode-hook 'highlight-indentation-current-column-mode)
+
 (provide 'init-yaml)
 ;;; init-yaml.el ends here
